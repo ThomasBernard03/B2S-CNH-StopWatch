a6da0d742930519c0e2fdda96dcca4d9192101a8
Thomas Bernard <thomas.bernard@ikmail.com>
2026-07-16 20:59:30 +0200

    chore: bump version 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "fr.b2s.cnhstopwatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
